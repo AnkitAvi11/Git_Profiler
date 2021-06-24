f1304cec04e1b5e4c453eaec2fbee7bba2f05da5
unknown <kumar.ankit383@gmail.com>
2021-06-24 09:32:42 +0530

    Removed bugs

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,7 +4,7 @@ class Stats extends Component {
     render() { 
         return (
             <div style={{textAlign:"center", marginBottom : "50px"}}>
-                <img src={`https://github-readme-stats.vercel.app/api?username=${this.props.username}&show_icons=true&count_private=true`}/> | <img src={`https://github-readme-streak-stats.herokuapp.com/?user=${this.props.username}`}/>
+                <img src={`https://github-readme-stats.vercel.app/api?username=${this.props.username}&show_icons=true&count_private=true`} alt="profile state" /> | <img src={`https://github-readme-streak-stats.herokuapp.com/?user=${this.props.username}`} alt="another state" />
             </div>
         )
     }
